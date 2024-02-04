61a15d07bc9973bd707adadb8c6207f0ae387150
divyanshu-in <cdivyanshu7@gmail.com>
2024-02-05 01:25:45 +0530

    feat: add test module

---
+++ tests/scripts/top_tokens.py
@@ -0,0 +1,13 @@
+import unittest
+from scripts.top_tokens import get_top_token
+
+
+class TestTopTokens(unittest.TestCase):
+
+    def test_get_top_token(self):
+        top_30_tokens = get_top_token(30)
+        self.assertTrue(len(top_30_tokens) == 30)
+
+
+if __name__ == "__main__":
+    unittest.main()
