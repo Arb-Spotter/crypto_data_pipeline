e3e42734e7c3a49d39db3c381a8237a54fef38d8
divyanshu-in <cdivyanshu7@gmail.com>
2024-02-10 00:41:47 +0530

    chore: use getattr

--- scripts/top_tokens.py
+++ scripts/top_tokens.py
@@ -1,25 +1,39 @@
 import ccxt
 
 
-def get_top_token(limit):
-    exchange = ccxt.binance()
+def get_top_token(exchange1, exchange2, limit):
+    exchange = getattr(ccxt, exchange1)()
 
     markets = exchange.load_markets()
 
-    token_pairs = [pair for pair, data in markets.items() if data['quote'] == 'USDT']
+    token_pairs = [pair for pair, data in markets.items() if data["quote"] == "USDT"]
 
-    sorted_tokens = sorted(token_pairs, key=lambda pair: markets[pair].get('quoteVolume') or -1, reverse=True)
+    sorted_tokens = sorted(
+        token_pairs,
+        key=lambda pair: markets[pair].get("quoteVolume") or -1,
+        reverse=True,
+    )
     top_30_tokens_from_binance = [token.split("/")[0] for token in sorted_tokens]
-    
-    exchange = ccxt.kraken()
+
+    exchange = getattr(ccxt, exchange2)()
 
     markets = exchange.load_markets()
-    token_pairs = [pair for pair, data in markets.items() if data['quote'].startswith('USDT')]
+    token_pairs = [
+        pair for pair, data in markets.items() if data["quote"].startswith("USDT")
+    ]
 
-    sorted_tokens = sorted(token_pairs, key=lambda pair: markets[pair].get('quoteVolume') or -1, reverse=True)
+    sorted_tokens = sorted(
+        token_pairs,
+        key=lambda pair: markets[pair].get("quoteVolume") or -1,
+        reverse=True,
+    )
     top_30_tokens_from_kraken = [token.split("/")[0] for token in sorted_tokens]
 
-    common_tokens = [value for value in top_30_tokens_from_binance if value in top_30_tokens_from_kraken]
+    common_tokens = [
+        value
+        for value in top_30_tokens_from_binance
+        if value in top_30_tokens_from_kraken
+    ]
 
     top_tokens = []
 
@@ -28,6 +42,9 @@ def get_top_token(limit):
         if poped_token not in top_tokens:
             top_tokens.append(poped_token)
 
-    print(f"Total of {len(top_tokens)} \nTokens - {top_tokens}")
+    return top_tokens[:limit]
 
-    return top_tokens
+
+if __name__ == "__main__":
+    tokens = get_top_token(30)
+    print(f"Total Tokens - {tokens} : {len(tokens)}")
